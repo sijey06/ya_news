c624b67d58125378664f6eb3eee1b94f95c93fce
Igor Zhuravlev <sijey06@mail.ru>
2025-03-21 00:13:28 +0500

    ver 1

---
+++ news/tests/dlya testov.py
@@ -0,0 +1,28 @@
+from django.test import TestCase
+
+# Импортируем модель, чтобы работать с ней в тестах.
+from news.models import News
+
+
+# Создаём тестовый класс с произвольным названием, наследуем его от TestCase.
+class TestNews(TestCase):
+    # Все нужные переменные сохраняем в атрибуты класса.
+    TITLE = 'Заголовок новости'
+    TEXT = 'Тестовый текст'
+    
+    @classmethod
+    def setUpTestData(cls):
+        cls.news = News.objects.create(
+            # При создании объекта обращаемся к константам класса через cls.
+            title=cls.TITLE,
+            text=cls.TEXT,
+        )
+
+    def test_successful_creation(self):
+        news_count = News.objects.count()
+        self.assertEqual(news_count, 1)
+
+    def test_title(self):
+        # Чтобы проверить равенство с константой -
+        # обращаемся к ней через self, а не через cls:
+        self.assertEqual(self.news.title, self.TITLE)
